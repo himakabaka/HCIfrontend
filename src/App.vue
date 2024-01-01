e7346c64a27e65cf2bf5bf9d7c5d3a5fa3449caa
YuXinbo <201250165@smail.nju.edu.cn>
2024-01-01 21:49:22 +0800

    “两个组件”

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <home data/>
+  <div>
+    <div v-for="(tuple, index) in tuples" :key="index">
+      <panel :title="tuple[1][0]" >
+        <div v-for="(value, key) in tuple[1]" :key="key">
+          <strong>{{ key }}:</strong> {{ value }}
+        </div>
+      </panel>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "@/components/myHome";
+import Panel from '@/components/myPanel.vue'; // 请根据你的项目路径调整
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    Panel
+  } ,
+  data() {
+    return {
+      jsonData: {
+        "2312.17248v1": {
+          "id": "2312.17248v1",
+          "url": "http://arxiv.org/abs/2312.17248v1",
+          "pdf": "http://arxiv.org/pdf/2312.17248v1",
+          "title": "Rethinking Model-based, Policy-based, and Value-based Reinforcement Learning via the Lens of Representation Complexity",
+          "abstract": "Reinforcement Learning (RL) encompasses diverse paradigms, including\nmodel-based RL, policy-based RL, and value-based RL, each tailored to\napproximate the model, optimal policy, and optimal value function,\nrespectively. This work investigates the potential hierarchy of representation\ncomplexity -- the complexity of functions to be represented -- among these RL\nparadigms. We first demonstrate that, for a broad class of Markov decision\nprocesses (MDPs), the model can be represented by constant-depth circuits with\npolynomial size or Multi-Layer Perceptrons (MLPs) with constant layers and\npolynomial hidden dimension. However, the representation of the optimal policy\nand optimal value proves to be $\\mathsf{NP}$-complete and unattainable by\nconstant-layer MLPs with polynomial size. This demonstrates a significant\nrepresentation complexity gap between model-based RL and model-free RL, which\nincludes policy-based RL and value-based RL. To further explore the\nrepresentation complexity hierarchy between policy-based RL and value-based RL,\nwe introduce another general class of MDPs where both the model and optimal\npolicy can be represented by constant-depth circuits with polynomial size or\nconstant-layer MLPs with polynomial size. In contrast, representing the optimal\nvalue is $\\mathsf{P}$-complete and intractable via a constant-layer MLP with\npolynomial hidden dimension. This accentuates the intricate representation\ncomplexity associated with value-based RL compared to policy-based RL. In\nsummary, we unveil a potential representation complexity hierarchy within RL --\nrepresenting the model emerges as the easiest task, followed by the optimal\npolicy, while representing the optimal value function presents the most\nintricate challenge.",
+          "authors": [
+            "Guhao Feng",
+            "Han Zhong"
+          ],
+          "update_time": "2023-12-29 02:59:49",
+          "summary": null
+        },
+        "2312.17248v2": {
+          "id": "2312.17248v1",
+          "url": "http://arxiv.org/abs/2312.17248v1",
+          "pdf": "http://arxiv.org/pdf/2312.17248v1",
+          "title": "Rethinking Model-based, Policy-based, and Value-based Reinforcement Learning via the Lens of Representation Complexity",
+          "abstract": "Reinforcement Learning (RL) encompasses diverse paradigms, including\nmodel-based RL, policy-based RL, and value-based RL, each tailored to\napproximate the model, optimal policy, and optimal value function,\nrespectively. This work investigates the potential hierarchy of representation\ncomplexity -- the complexity of functions to be represented -- among these RL\nparadigms. We first demonstrate that, for a broad class of Markov decision\nprocesses (MDPs), the model can be represented by constant-depth circuits with\npolynomial size or Multi-Layer Perceptrons (MLPs) with constant layers and\npolynomial hidden dimension. However, the representation of the optimal policy\nand optimal value proves to be $\\mathsf{NP}$-complete and unattainable by\nconstant-layer MLPs with polynomial size. This demonstrates a significant\nrepresentation complexity gap between model-based RL and model-free RL, which\nincludes policy-based RL and value-based RL. To further explore the\nrepresentation complexity hierarchy between policy-based RL and value-based RL,\nwe introduce another general class of MDPs where both the model and optimal\npolicy can be represented by constant-depth circuits with polynomial size or\nconstant-layer MLPs with polynomial size. In contrast, representing the optimal\nvalue is $\\mathsf{P}$-complete and intractable via a constant-layer MLP with\npolynomial hidden dimension. This accentuates the intricate representation\ncomplexity associated with value-based RL compared to policy-based RL. In\nsummary, we unveil a potential representation complexity hierarchy within RL --\nrepresenting the model emerges as the easiest task, followed by the optimal\npolicy, while representing the optimal value function presents the most\nintricate challenge.",
+          "authors": [
+            "Guhao Feng",
+            "Han Zhong"
+          ],
+          "update_time": "2023-12-29 02:59:49",
+          "summary": null
+        }
+      }
+    };
+  },
+  computed: {
+    tuples() {
+      // Convert JSON data to array of tuples
+      return Object.entries(replaceKeywordsWithChinese(this.jsonData));
+    }
+}}
+
+
+function replaceKeywordsWithChinese(jsonData) {
+  const replacedData = {};
+
+  for (const key in jsonData) {
+
+      const item = jsonData[key];
+      const replacedItem = {
+        "编号": item.id,
+        "链接": item.url,
+        "PDF链接": item.pdf,
+        "标题": item.title,
+        "摘要": item.abstract,
+        "作者": item.authors,
+        "更新时间": item.update_time,
+        "概述": item.summary
+      };
+
+      replacedData[key] = replacedItem;
+
   }
+
+  return replacedData;
 }
 </script>
 
@@ -19,8 +91,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
 }
 </style>
---
+++ src/components/myPanel.vue
@@ -0,0 +1,59 @@
+<!-- Panel.vue -->
+
+<template>
+  <div class="panel">
+    <div class="panel-header">
+        {{ title + author}}
+
+    </div>
+    <div class="panel-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import {List} from "@opentiny/vue";
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    author:{
+      type:List,
+      required: true
+    }
+
+  }
+};
+</script>
+
+<style scoped>
+.panel {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 10px auto;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease-in-out;
+}
+
+.panel:hover {
+  transform: scale(1.02);
+}
+
+.panel-header {
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px;
+  font-size: 1.2em;
+  border-bottom: 1px solid #ddd;
+  border-radius: 8px 8px 0 0;
+}
+
+.panel-content {
+  padding: 20px;
+}
+</style>
